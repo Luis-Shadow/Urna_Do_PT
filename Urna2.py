dd88a7a2ccb477b676ffb4f9a41c6f09da446fe1
Luis-Shadow <kazuma.h.satou@gmail.com>
2026-09-10 21:41:13 -0300

    Ultimas modificações
    Falta só mostrar quem tem mais votos e anunciar o vencedor.

--- Urna2.py
+++ Urna2.py
@@ -4,24 +4,33 @@
 import json
 import os
 os.system("cls")
-candidatos = {
-    #Kratos
-    "1234": 0,
-    #Kazuma
-    "5678": 0,
-    #Branco
-    "0": 0
-}
+candidatos = [
+    #Políticos
+    [1234, 5678,8901, 0000],
+    #O número de votos
+    [0,0,0,0],
+    #Nome dos políticos
+    ["Kazuma","Kratos","Salsicha","Branco"]
+]
 def resultado():
-    pass
-f = open("Data.json", "a")
+    total = 0
+    for x in range(len(candidatos[0])):
+        total += candidatos[1][x]
+    print("Resultado da eleição:\n")
+    for x in range(len(candidatos[0])):
+        print(f"Candidato: {candidatos[2][x]} | {candidatos[0][x]}\n    Número de votos: {candidatos[1][x]} com {(candidatos[1][x]/total)*100:.2f}% dos votos\n")
+    
 while True:
     try:
-        candidatos[input('Voto: ')] += 1
+        voto = int(input("Número do candidato: "))
+        posicao = candidatos[0].index(voto)
+        candidatos[1][posicao] +=1
     except ValueError:
         print("Digite um número seu(a) animal!")
     except KeyError:
         print("Candidato não encontrado, se quiser votar branco digite 0")
     except KeyboardInterrupt:
-        pass
-    f.close()
+        os.system("cls")
+        os.system("color 2")
+        resultado()
+        break
