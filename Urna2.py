8feaa1191cca0e95236dcb7b2818a8449c9f4f8e
Luis-Shadow <kazuma.h.satou@gmail.com>
2026-09-12 19:18:12 -0300

    Finalização_da_Urna
    Se quiser, pode roubar já que a urna é manipulável (interprete como quiser).

--- Urna2.py
+++ Urna2.py
@@ -1,11 +1,7 @@
-#Tem que fazer um contador para votos de cada candidato.
-#Colocar os candidatos em uma lista e transformar em um objeto json.
-#Fazer uma função que defina a porcentagem de voto e o vencedor da eleição.
-import json
 import os
 os.system("cls")
 candidatos = [
-    #Políticos
+    #Id dos políticos
     [1234, 5678,8901, 0000],
     #O número de votos
     [0,0,0,0],
@@ -16,15 +12,23 @@ def resultado():
     total = 0
     for x in range(len(candidatos[0])):
         total += candidatos[1][x]
+        global flag_vencedor
     print("Resultado da eleição:\n")
     for x in range(len(candidatos[0])):
         print(f"Candidato: {candidatos[2][x]} | {candidatos[0][x]}\n    Número de votos: {candidatos[1][x]} com {(candidatos[1][x]/total)*100:.2f}% dos votos\n")
+    print("Vencedor: ", candidatos[2][flag_vencedor])
     
 while True:
     try:
         voto = int(input("Número do candidato: "))
         posicao = candidatos[0].index(voto)
         candidatos[1][posicao] +=1
+        flag_vencedor = 0
+        flag_maior = 0
+        for x in range(len(candidatos[1])):
+            if candidatos[1][x] > flag_maior:
+                flag_maior += 1
+                flag_vencedor = candidatos[1].index(candidatos[1][x])
     except ValueError:
         print("Digite um número seu(a) animal!")
     except KeyError:
